b6e28703c97bf700e34f0c92edefe75961e4c4ab
ak352 <ak352319@gmail.com>
2024-04-13 23:11:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "genai Assistant Project",
+    version="0.0.0",
+    author="Abhishek kumar",
+    author_email="ak352319@gmail.com",
+    packages=find_packages(),
+)
